b83d1f7ac3b713def275e5b9b1847f7c0593c769
zhaobing <zhaobing024@gmail.com>
2015-11-17 22:59:10 +0800

    这里需要将那些剧目曾经看过做一个记录，以便能够了解哪些电视剧是看过的！

--- app/models/WatchlistTraitForUser.php
+++ app/models/WatchlistTraitForUser.php
@@ -13,11 +13,12 @@ trait WatchlistTraitForUser
     public function getMyWatchListMovies()
     {
         if([] == $this->watchListMovies){
+            $auth = \Phalcon\Di::getDefault()->get('auth');
             $this->watchListMovies = Watchlist::query()
 //                ->leftJoin('Users','Users.id = Watchlist.user_id ')
                 ->leftJoin('Movies','Movies.id = Watchlist.movie_id')
                 ->leftJoin('Episodes','Episodes.id = Watchlist.currentEpisode_id')
-                ->where('Watchlist.user_id = :user:',['user'=>\Phalcon\Di::getDefault()->get('auth')->id])
+                ->where('Watchlist.user_id = :user:',['user'=>$auth->id])
                 ->andWhere('Watchlist.status != "done" ')
                 ->orderBy('Watchlist.updated_at DESC')
                 ->columns(['Watchlist.*','Movies.*','Episodes.*'])
